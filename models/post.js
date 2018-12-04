d331be06995af720a3a894ba9f73bd6937209276
Severiano Badajoz <sevebadajoz@gmail.com>
2018-12-04 11:37:09 -0800

    Add subreddits to posts

--- models/post.js
+++ models/post.js
@@ -13,7 +13,11 @@ const PostSchema = new Schema({
     summary: {
         type: String,
         required: true
-    }
+    },
+    subreddit: {
+      type: String,
+      required: true,
+    },
 });
 
 PostSchema.pre('save', function(next) {
